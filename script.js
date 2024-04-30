91c75b719fcf43da37b1189fd640075575bf9075
Md. Tauhid Islam Rafi <166207955+engr-tauhid@users.noreply.github.com>
2024-04-30 07:52:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const fastName = 'jonas';
+const lastName = " hel";
+const concanate = `hi my name's ${fastName} ${ lastName}`
+console.log(concanate);
+if (10==21)(
+    console.log(`fake`)
+)
+else if (10==10)
+{
+console.log(34)}else {
+    console.log(23)
+}
